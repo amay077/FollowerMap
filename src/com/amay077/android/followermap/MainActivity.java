94f19e92e66f2de61aa74758e90c0431d8e1dff7
amay077 <okuokuoku@gmail.com>
2010-12-04 11:05:17 +0900

    ハマッカソンようにブランチ切った

--- src/com/amay077/android/followermap/MainActivity.java
+++ src/com/amay077/android/followermap/MainActivity.java
@@ -1,31 +1,13 @@
 package com.amay077.android.followermap;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import com.amay077.android.followermap.R;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
-import com.vividsolutions.jts.index.SpatialIndex;
-import com.vividsolutions.jts.index.quadtree.Quadtree;
-
-import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.os.AsyncTask.Status;
-import android.util.Log;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.ProgressBar;
 
-public class MainActivity extends MapActivity implements Progressable, TaskFactory  {
+public class MainActivity extends MapActivity {
 
-	private LoadingTask fatTask = null;
-	private ProgressBar prgBar = null;
 	private MapView mapview = null;
-	private Map<String, GeoPointWithInfo> addrGeoMap = new HashMap<String, GeoPointWithInfo>();
-	private Map<Integer, Bitmap> userImageMap = new HashMap<Integer, Bitmap>();
-	private SpatialIndex spIndex = new Quadtree();
 
 	/** Called when the activity is first created. */
     @Override
@@ -33,60 +15,14 @@ public class MainActivity extends MapActivity implements Progressable, TaskFacto
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        prgBar = (ProgressBar)findViewById(R.id.ProgressBar01);
-
         mapview = (MapView)findViewById(R.id.mapView);
         mapview.setBuiltInZoomControls(true);
 
-        final TweetOverlay overlay = new TweetOverlay(this);
-        mapview.getOverlays().add(overlay);
         mapview.getOverlays().add(new GeoHexOverlay());
-
-        Button btn = (Button) findViewById(R.id.Button01);
-        btn.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				Log.d(this.getClass().toString(), "onClick");
-
-				fatTask = CreateTask();
-				fatTask.execute();
-			}
-		});
-
-        btn = (Button) findViewById(R.id.Button02);
-        btn.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				Log.d(this.getClass().toString(), "onCancel");
-
-				if (fatTask != null && fatTask.getStatus() == Status.RUNNING) {
-					fatTask.cancel(true);
-					fatTask = null;
-				}
-			}
-		});
 }
 
 	@Override
 	protected boolean isRouteDisplayed() {
 		return false;
-	}
-
-	@Override
-	public void onProgress(float percent) {
-		prgBar.setVisibility(percent == 100f ? View.INVISIBLE  : View.VISIBLE);
-	}
-
-	@Override
-	public LoadingTask CreateTask() {
-		if (fatTask != null && fatTask.getStatus() == Status.RUNNING) {
-			fatTask.cancel(true);
-			fatTask = null;
-		}
-
-		fatTask = new LoadingTask(mapview, this, addrGeoMap, userImageMap, spIndex);
-		return fatTask;
 	}
 }
